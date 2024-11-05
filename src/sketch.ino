421751ad65b7f5cdfe2dc0403b3dc15b7ea5d2de
Lucas Fonseca <lucasfowcosta@gmail.com>
2024-11-05 18:36:24 -0300

    terceiro commit sensor hcsr04

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,27 +1,47 @@
 #include <DHT.h>
 
 
-const int pinoDHT = 23;
+int pinoDHT =  23;
+int trigPin =  22;
+int echoPin =  21;
+
+
 DHT dht(pinoDHT, DHT22);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  dht.begin();
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
 }
 
 void loop() {
-  // pega as informações de temperatura e umidade do sensor dht
   float temp = dht.readTemperature();
   float umi = dht.readHumidity();
+
+  
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, LOW);
+  
+  long duration = pulseIn(echoPin,HIGH);
+  int distance = duration * 0.034/2;
+
   Serial.print("Temperatura: ");
   Serial.print(temp);
   Serial.println("°C");
   Serial.print("Umidade: ");
   Serial.print(umi);
   Serial.println("%");
-  Serial.println("-------");
 
-  
-  delay(3000);
+
+  Serial.print("Distancia: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  delay(1000); // this speeds up the simulation
 }
