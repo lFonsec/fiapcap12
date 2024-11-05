709cd1638d651c20d29dd7672ba6d97b388de9f4
Lucas Fonseca <lucasfowcosta@gmail.com>
2024-11-05 18:54:03 -0300

    sensor pir

--- src/sketch.ino
+++ src/sketch.ino
@@ -4,7 +4,10 @@
 int pinoDHT =  23;
 int trigPin =  22;
 int echoPin =  21;
-
+int pirLED  = 19;
+int inputPin = 34;
+int buzzerPin = 5;
+int buttonPin = 17;
 
 DHT dht(pinoDHT, DHT22);
 
@@ -14,6 +17,10 @@ void setup() {
 
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  pinMode(inputPin, INPUT);
+  pinMode(pirLED, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
 
 }
 
@@ -21,13 +28,16 @@ void loop() {
   float temp = dht.readTemperature();
   float umi = dht.readHumidity();
 
+  int val = digitalRead(inputPin); 
+  int buttonState = HIGH;
+
   
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(2);
   digitalWrite(trigPin, LOW);
-  
+
   long duration = pulseIn(echoPin,HIGH);
   int distance = duration * 0.034/2;
 
@@ -42,6 +52,19 @@ void loop() {
   Serial.print("Distancia: ");
   Serial.print(distance);
   Serial.println(" cm");
+
+
+    if(val == HIGH){
+    digitalWrite(pirLED, HIGH);
+    while(buttonState == HIGH){
+      tone(buzzerPin, 1500);
+      Serial.println("Movimento Detectado ");
+      buttonState = digitalRead(buttonPin);
+    }
+    noTone(buzzerPin);
+    digitalWrite(pirLED, LOW);
+  }
+
 
   delay(1000); // this speeds up the simulation
 }
